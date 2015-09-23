8bc19970490c3bff9ef99c856adae4cf5b85f20b
perubau <yeiji2002@yahoo.co.jp>
2015-09-23 06:25:28 +0000

    editAndupdate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   
   def show # 追加
    @user = User.find(params[:id])
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to signup_path , notice: '個人情報を編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
   end
   
   def new
@@ -24,4 +38,8 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
